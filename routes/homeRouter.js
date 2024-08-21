6709864d00c5141f64708752369deba5e17271b7
qkrtjsrbs315 <qkrtjsrbs315@gmail.com>
2024-08-21 15:11:49 +0900

    Feat : Nyam&Recommend Product feature

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -2,7 +2,7 @@ import express from 'express';
 import passport from 'passport';
 import { response } from '../config/response.js';
 import { successStatus } from '../config/successStatus.js';
-import { getHome,getProductByCategoryID } from '../services/homeService.js';
+import { getHome,getProductByCategoryID,getNyamRecommend,getRecommend } from '../services/homeService.js';
 
 const homeRouter = express.Router();
 
@@ -12,9 +12,7 @@ homeRouter.get('/', async (req, res) => {
     //main_cateogryID와 cateogry를 통해 가져올 count
     const { count } = req.query;
     console.log(count)
-    
-    let query = '';
-    let params = [];
+
 
     try {
         const homeMainResponse = await getHome(count);
@@ -29,9 +27,6 @@ homeRouter.get('/category', async (req, res) => {
     const { category, count } = req.query;
     console.log(category,count)
     
-    let query = '';
-    let params = [];
-
     try {
         const homeMainResponse = await getProductByCategoryID(category,count);
         res.send(response(successStatus.HOME_SUCCESS, homeMainResponse));
@@ -40,5 +35,24 @@ homeRouter.get('/category', async (req, res) => {
         res.status(500).json({ error: 'Failed to fetch products' });
     }
 });
-
+homeRouter.get('/nyam', async (req, res) => {
+    try {
+        const { count } = req.query;
+        const homeMainResponse = await getNyamRecommend(count);
+        res.send(response(successStatus.HOME_SUCCESS, homeMainResponse));
+    } catch (err) {
+        console.error("Error fetching products:", err);
+        res.status(500).json({ error: 'Failed to fetch products' });
+    }
+});
+homeRouter.get('/recommend', async (req, res) => {
+    try {
+        const { count } = req.query;
+        const homeMainResponse = await getRecommend(count);
+        res.send(response(successStatus.HOME_SUCCESS, homeMainResponse));
+    } catch (err) {
+        console.error("Error fetching products:", err);
+        res.status(500).json({ error: 'Failed to fetch products' });
+    }
+});
 export default homeRouter;
